a45a85ee14d3543787820aa52221375384d37013
Rishi Krishna <rishikrishna9692@gmail.com>
2026-02-18 16:24:14 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,12 +5,10 @@ public class PalindromeCheckerApp {
         String original = "radar";
         String reversed = "";
 
-        // Step 1: Iterate the string in reverse order and build the new string
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed += original.charAt(i);
         }
 
-        // Step 2: Compare original and reversed strings
         System.out.println("Original String: " + original);
         System.out.println("Reversed String: " + reversed);
 
