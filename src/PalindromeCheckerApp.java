de51af04896e40944b0c719a0b8a7da32681b6c6
Rishi Krishna <rishikrishna9692@gmail.com>
2026-02-18 16:23:43 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,5 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        // Displaying welcome message and version as per requirements
+    public static void main(String[] args) 
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Application Version: 1.0");
         System.out.println("---------------------------------------------------");
