5da8fda67f2d52dc94a5bdf6d77aa35d5d534965
Rishi-ks666 <rishikrishna9692@gmail.com>
2026-02-26 15:56:41 +0530

    base application setup

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,42 +3,6 @@ import java.util.Stack;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        PalindromeStrategy strategy = new StackStrategy();
-        String testInput = "level";
 
-        long startTime = System.nanoTime();
-        boolean isPalindrome = strategy.check(testInput);
-        long endTime = System.nanoTime();
-
-        long duration = endTime - startTime;
-
-        System.out.println("Input : " + testInput);
-        System.out.println("Is Palindrome? : " + isPalindrome);
-        System.out.println("Execution Time : " + duration + " ns");
-    }
-}
-
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
-        if (input == null) return false;
-
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
